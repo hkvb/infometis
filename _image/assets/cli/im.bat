d7c56723fc55a4fdc3c2a3a2243c618652b415bc
hvb <herman@vcweb.co>
2021-07-15 14:42:59 +0000

    implemented console with accepting single command

--- _image/assets/cli/im.bat
+++ _image/assets/cli/im.bat
@@ -1,2 +1,2 @@
 
-docker run -it -v /etc/docker/certs.d:/etc/docker/certs.d:ro -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v vcw_controlplane_repos:/vcw/pwd ${VCW_REGISTRY}hkvb/infometis --container %*  >nul
+sudo docker run -it -v /etc/docker/certs.d:/etc/docker/certs.d:ro -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v vcw_controlplane_repos:/vcw/pwd ${VCW_REGISTRY}hkvb/infometis --container %*  >nul
