e0cef937a71a7c1a45a89105ecd2160b2e6d4d40
hvb <herman@vcweb.co>
2021-07-25 17:37:27 +0000

    bug fixes

--- _image/assets/cli/create-infometis-volume.bat
+++ _image/assets/cli/create-infometis-volume.bat
@@ -1,4 +1,4 @@
 
-sudo docker container prune --force
-sudo docker volume rm vcw_infometis_repos
-sudo docker volume create --driver local --opt type=none --opt o=bind --opt device=%cd% vcw_infometis_repos
+docker container prune --force
+docker volume rm vcw_infometis_repos
+docker volume create --driver local --opt type=none --opt o=bind --opt device=%cd% vcw_infometis_repos
--- _image/assets/cli/infometis.bat
+++ _image/assets/cli/infometis.bat
@@ -1,3 +1,3 @@
 
 echo "Starting infometis"
-sudo docker run -it -v /etc/docker/certs.d:/etc/docker/certs.d:ro -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v vcw_controlplane_repos:/vcw/pwd ${VCW_REGISTRY}hkvb/infometis --windows %*
+docker run -it -v /etc/docker/certs.d:/etc/docker/certs.d:ro -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v vcw_infometis_repos:/vcw/pwd ${VCW_REGISTRY}hkvb/infometis --windows %*
